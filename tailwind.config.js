c99c8d8bfcbd25776020a96e45a03c8f7c2ffbf3
Anuj Kumar <87929996+anujkharbash2@users.noreply.github.com>
2025-04-26 13:23:25 +0530

    1

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          50: '#eef2ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          800: '#172554',
+          900: '#0c193d',
+        },
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            a: {
+              color: theme('colors.blue.600'),
+              '&:hover': {
+                color: theme('colors.blue.800'),
+              },
+            },
+            h1: {
+              color: theme('colors.blue.900'),
+            },
+            h2: {
+              color: theme('colors.blue.900'),
+            },
+            h3: {
+              color: theme('colors.blue.900'),
+            },
+            h4: {
+              color: theme('colors.blue.900'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [],
+};
